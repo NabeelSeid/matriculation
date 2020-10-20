26e620f83710acc40ddbc447363356197a704c44
Nabil Seid <nabeelseid@gmail.com>
2020-10-20 11:36:01 +0300

    router added

---
+++ lib/router.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:matric/routes.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case Routes.Home:
+      // MaterialPageRoute(builder: (_) => HomeView());
+      return null;
+    case Routes.Download:
+      //TODO download route
+      return null;
+    case Routes.Subject:
+      //TODO subject route
+      return null;
+    case Routes.Exam:
+      //TODO exam route
+      return null;
+    case Routes.ExamPractice:
+      //TODO exam practice route
+      return null;
+    case Routes.ExamSim:
+      //TODO exam simulation route
+      return null;
+    case Routes.ExamChap:
+      //TODO exam chapterized route
+      return null;
+  }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const String Home = '/';
+  static const String Download = '/download';
+  static const String Subject = '/subject';
+  static const String Exam = '/exam';
+  static const String ExamPractice = '/exam/practice';
+  static const String ExamSim = '/exam/sim';
+  static const String ExamChap = '/exam/chap';
+}
