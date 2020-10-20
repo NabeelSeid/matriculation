b6c842d7c93aa6c85f97af046df0cf706561e6a7
Nabil Seid <nabeelseid@gmail.com>
2020-10-20 15:39:18 +0300

    BarButton refactored

---
+++ lib/ui/widgets/bar_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BarButton extends StatelessWidget {
+  const BarButton({
+    Key key,
+    this.elevation: 3,
+    @required this.borderRadius,
+    @required this.child,
+    this.onTab,
+  }) : super(key: key);
+
+  final double elevation;
+  final BorderRadius borderRadius;
+  final Widget child;
+  final Function onTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.card,
+      elevation: elevation,
+      borderRadius: borderRadius,
+      child: InkWell(
+        onTap: () {},
+        borderRadius: borderRadius,
+        child: child,
+      ),
+    );
+  }
+}
