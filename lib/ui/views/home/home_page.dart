877cb34eea23a91d50734499c7d7e1ef4d601626
Nabil Seid <nabeelseid@gmail.com>
2020-10-21 15:15:59 +0300

    changed homePage to stateless

--- lib/ui/views/home/home_page.dart
+++ lib/ui/views/home/home_page.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   HomePage({Key key, this.children, this.paddingBottom: 10}) : super(key: key);
 
   final List<Widget> children;
   final double paddingBottom;
 
   @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.fromLTRB(10, 60, 10, 0),
         child: Column(
-          children: widget.children
+          children: children
               .map((e) => Padding(
-                    padding: EdgeInsets.only(bottom: widget.paddingBottom),
+                    padding: EdgeInsets.only(bottom: paddingBottom),
                     child: e,
                   ))
               .toList(),
