fffa2c46f874cffc3e6aff78edf8632909347c60
Nabil Seid <nabeelseid@gmail.com>
2020-10-21 15:01:46 +0300

    added examExpansionTile widget

--- lib/demo.dart
+++ lib/demo.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:matric/ui/shared/border_shape.dart';
 import 'package:matric/ui/shared/colors.dart';
 import 'package:matric/ui/shared/font_styles.dart';
+import 'package:matric/ui/views/home/exam_expansion_tile.dart';
+import 'package:matric/ui/widgets/subject_button.dart';
 
 class Demo extends StatefulWidget {
   Demo({Key key}) : super(key: key);
@@ -19,9 +21,41 @@ class _DemoState extends State<Demo> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
-          child: Stack(
+          child: ExamExpansionTile(
+            title: Text('2008'),
             children: [
-              // SubjectBarButton(),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: SubjectButton(
+                      subject: 'Chemistry',
+                      year: '2008',
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: SubjectButton(
+                      subject: 'Chemistry',
+                      year: '2008',
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: SubjectButton(
+                      subject: 'Chemistry',
+                      year: '2008',
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: SubjectButton(
+                      subject: 'Chemistry',
+                      year: '2008',
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
--- lib/ui/shared/border_shape.dart
+++ lib/ui/shared/border_shape.dart
@@ -5,3 +5,5 @@ enum border_shape { Boxy, SmoothCurve, Rounded }
 final BorderRadius boxy = BorderRadius.circular(5.0);
 final BorderRadius smoothCurve = BorderRadius.circular(10.0);
 final BorderRadius rounded = BorderRadius.circular(20.0);
+
+final double elevation = 3;
