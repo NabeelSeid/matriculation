16b3a349e7e8b6e6570d49bc69aee2bf03e9b580
Nabil Seid <nabeelseid@gmail.com>
2020-10-27 13:31:51 +0300

    added downloadModel

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,13 @@
 import 'package:get_it/get_it.dart';
+import 'package:matric/core/models/download_model.dart';
 import 'package:matric/core/services/navigation_service.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
+  // Models
+  locator.registerLazySingleton(() => DownloadModel());
+
+  // Services
   locator.registerLazySingleton(() => NavigationService());
 }
